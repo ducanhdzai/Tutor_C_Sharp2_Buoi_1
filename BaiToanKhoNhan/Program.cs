0e96681c54f49941833ab4ac335ecf154408176f
Anh Nguyễn Văn Đức <anhnvd.job@gmail.com>
2023-06-05 23:07:41 +0700

    Tutor C#2 Buoi 3

---
+++ BaiToanKhoNhan/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaiToanKhoNhan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Moi ban nhap ten: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Moi ban nhap msv: ");
+            string msv = Console.ReadLine() + "@fpt.edu.vn";
+            List<string> lstStr = name.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList(); // Cắt theo kí tự nào đấy
+            // .Trim dung de cat bo ki tu khoang trang o dau va cuoi
+            // VD: Nguyen Van Duc Anh
+            string email = lstStr[lstStr.Count - 1];
+            // email = "Anh"
+            for (int i = 0; i < lstStr.Count - 1; i++)
+            {
+                // Lay chu cai dau tien cua thanh phan trong ten
+                email += lstStr[i].Substring(0, 1);
+            }
+            // email = anhnvd
+            email = RemoveUnicode(email.ToLower());
+            Console.WriteLine((email + msv).ToLower());
+
+        }
+        public static string RemoveUnicode(string str)
+        {
+            List<string> listUnicode = new List<string>() { "á", "à", "ả", "ã", "ạ", "â", "ấ", "ầ", "ẩ", "ẫ", "ậ", "ă", "ắ", "ằ", "ẳ", "ẵ", "ặ",
+    "đ",
+    "é","è","ẻ","ẽ","ẹ","ê","ế","ề","ể","ễ","ệ",
+    "í","ì","ỉ","ĩ","ị",
+    "ó","ò","ỏ","õ","ọ","ô","ố","ồ","ổ","ỗ","ộ","ơ","ớ","ờ","ở","ỡ","ợ",
+    "ú","ù","ủ","ũ","ụ","ư","ứ","ừ","ử","ữ","ự",
+    "ý","ỳ","ỷ","ỹ","ỵ" };
+            List<string> listNonUnicode = new List<string>()
+            {
+                "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",
+    "d",
+    "e","e","e","e","e","e","e","e","e","e","e",
+    "i","i","i","i","i",
+    "o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o",
+    "u","u","u","u","u","u","u","u","u","u","u",
+    "y","y","y","y","y"};
+            for (int i = 0; i < listUnicode.Count; i++)
+            {
+                str = str.Replace(listUnicode[i], listNonUnicode[i]);
+            }
+            return str;
+        }
+    }
+}
---
+++ Tutor_C_Sharp_Buoi_3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Tutor_C_Sharp_Buoi_3
+{
+    internal class Program
+    {
+        static FileStream _fs;
+        static BinaryFormatter _bf = new BinaryFormatter();
+        static List<Student> lstStudent;
+        static void Main(string[] args)
+        {
+            // Khoi tao doi tuong de tuan tu hoa du lieu
+            // LINQ : LANGUE Intergrated Query -> Ngon ngu truy van
+            // LINQ dùng để sắp xếp,...
+            // Đi thi thì sẽ là dạng sắp xếp, where, 1 số thao tác đến chuỗi
+            string filePath = @"C:\Users\ADMIN\Desktop\C#2_Tutor\Tutor_C_Sharp2_Buoi_1\Tutor_C_Sharp_Buoi_3\data.bin";
+            lstStudent = new List<Student>()
+            {
+                new Student{ Id= Guid.NewGuid(), Name = "Hai Occho"}, // GUID là 1 đoạn string được random mục đích thường là để sử dụng làm khóa chính bởi vì nó rất rất khó để bị trùng lặp,.
+                new Student{ Id= Guid.NewGuid(), Name = "Duc Anh"},
+                new Student{ Id= Guid.NewGuid(), Name = "DinhQuynhTrang"},
+                new Student{ Id= Guid.NewGuid(), Name = "Mai Nguyen Long Vu"},
+                new Student{ Id= Guid.NewGuid(), Name = "Truong Van Hoang"},
+                new Student{ Id= Guid.NewGuid(), Name = "Dang Xuan Phong"},
+            };
+            try
+            {
+                SaveFile(filePath);
+                ReadFile(filePath);
+            }
+            catch (System.Exception e)
+            {
+
+                throw e;
+            }
+            Console.ReadKey();
+        }
+        public static void SaveFile(string filePath)
+        {
+            Console.WriteLine("Save File");
+            // Kiểm tra xem file đã tồn tại hay chưa
+            if (Directory.Exists(filePath))
+            {
+                _fs = new FileStream(filePath, FileMode.Create);// Tao ra file 
+                _bf.Serialize(_fs, lstStudent);
+                _fs.Close();// Đóng lại để còn làm việc khác
+            };
+        }
+        public static void ReadFile(string filePath)
+        {
+            Console.WriteLine("Read File");
+            if (!Directory.Exists(filePath))
+            {
+                _fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);// Tao ra file
+                var data = _bf.Deserialize(_fs) as List<Student>; // Ép kiểu dữ liệu
+                _fs.Close();// Đóng lại để còn làm việc khác
+                foreach (var item in data)
+                {
+                    Console.WriteLine("ID: " + item.Id);
+                    Console.WriteLine("Name: " + item.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Du lieu khong co");
+            }
+        }
+        [Serializable] // Cần phải đánh dấu là tuần tự hóa
+        public class Student
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+
+        }
+    }
+}
